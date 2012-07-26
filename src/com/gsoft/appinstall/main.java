489697200bcd4db9ab2fec0630b89a7e879c70df
tellen.yu <tellen.yu@amlogic.com>
2012-07-26 19:28:57 +0800

    shield path usbdrive

--- src/com/gsoft/appinstall/main.java
+++ src/com/gsoft/appinstall/main.java
@@ -329,7 +329,7 @@ public class main extends Activity {
                     String filename = arg0.getName();
                     String filenamelowercase = filename.toLowerCase();
                     if( (filenamelowercase.compareTo("asec")!=0) && (filenamelowercase.compareTo("secure")!=0) &&
-                        (filenamelowercase.compareTo("obb")!=0))
+                        (filenamelowercase.compareTo("obb")!=0)&&(filenamelowercase.compareTo("usbdrive")!=0))
                     {
                         return true;
                     }
