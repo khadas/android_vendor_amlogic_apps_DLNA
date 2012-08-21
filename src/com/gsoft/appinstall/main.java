5c57bd4cc10d2de2a88c06a6d62464cfbffccf47
tellen.yu <tellen.yu@amlogic.com>
2012-08-21 14:28:43 +0800

    enable real sdcard check

--- src/com/gsoft/appinstall/main.java
+++ src/com/gsoft/appinstall/main.java
@@ -111,7 +111,7 @@ public class main extends Activity {
         });
 
 		/* check whether use real sdcard*/
-		isRealSD = true; //Environment.isExternalStorageBeSdcard();
+		isRealSD = Environment.isExternalStorageBeSdcard();
         
         //change dir button
         m_DirEdit = (TextView)findViewById(R.id.Dir);
