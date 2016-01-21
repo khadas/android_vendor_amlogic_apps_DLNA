9e2012ce555a57f7e26ce2eca6696a5aaeb0e9a2
Zhiwei Gong <zhiwei.gong@amlogic.com>
2016-01-21 14:47:24 +0800

    PD#118443: fix airplay connot find device when the ip changed
    Change-Id: Icb7cc717c49f0a003e2924d9a4c98a57c22235e1

--- src/com/droidlogic/mediacenter/DMRBroadcastReceiver.java
+++ src/com/droidlogic/mediacenter/DMRBroadcastReceiver.java
@@ -30,7 +30,12 @@ import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.wifi.WifiManager;
+import android.os.Handler;
+import android.os.Message;
 import org.cybergarage.util.Debug;
+import android.util.Log;
+
+
 /**
  * @ClassName DMRBroadcastReceiver
  * @Description TODO
@@ -43,53 +48,50 @@ public class DMRBroadcastReceiver extends BroadcastReceiver {
         private PrefUtils mPrefUtils;
 
         private static final String TAG = "DMRBroadcastReceiver";
+        private static final int STOPSERVICE = 0;
+        private static final int STARTSERVICE = 1;
+        Context mContext;
         /* (non-Javadoc)
          * @see android.content.BroadcastReceiver#onReceive(android.content.Context, android.content.Intent)
          */
         @Override
         public void onReceive ( Context cxt, Intent intent ) {
+            mContext = cxt;
             mPrefUtils = new PrefUtils ( cxt );
             /*restart DMRservice when network config changed*/
+            Log.d ( TAG ,">>>>>onReceive : onReceive" + intent.getAction());
             if ( ( ConnectivityManager.CONNECTIVITY_ACTION ).equals ( intent.getAction() ) ) {
-                NetworkInfo netInfo = ( NetworkInfo ) intent.getExtra ( WifiManager.EXTRA_NETWORK_INFO, null );
                 ConnectivityManager cMgr = ( ConnectivityManager ) cxt.getSystemService ( Context.CONNECTIVITY_SERVICE );
-                if ( ( cMgr.getNetworkInfo ( ConnectivityManager.TYPE_WIFI ) != null ) && !cMgr.getNetworkInfo ( ConnectivityManager.TYPE_WIFI ).isConnectedOrConnecting()
-                        || ( ( cMgr.getNetworkInfo ( ConnectivityManager.TYPE_ETHERNET ) != null ) ) && !cMgr.getNetworkInfo ( ConnectivityManager.TYPE_ETHERNET ).isConnectedOrConnecting() ) {
-                    Intent mNetIntent = new Intent ( DmpService.NETWORK_ERROR );
-                    cxt.sendBroadcast ( mNetIntent );
-                    //cxt.stopService(new Intent(cxt,MediaCenterService.class));
-                    Debug.d ( TAG , ">>>>>onReceive :network disconnected" );
-                    if ( AirPlayService.STATEDFLAG && mPrefUtils.getBooleanVal ( SettingsPreferences.KEY_BOOT_CFG, false ) ) {
-                        cxt.stopService ( new Intent ( cxt, AirPlayService.class ) );
+                NetworkInfo netInfo = ( NetworkInfo ) intent.getExtra ( ConnectivityManager.EXTRA_NETWORK_INFO, null );
+                NetworkInfo netInfoWIFI = cMgr.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+                NetworkInfo netInfoETH = cMgr.getNetworkInfo(ConnectivityManager.TYPE_ETHERNET);
+
+                if (netInfo != null) {
+                    if (netInfo.getState() == NetworkInfo.State.CONNECTED) {
+                        Log.d ( TAG ,"TypeName " + netInfo.getTypeName()+ " connected");
+                        mHandler.sendEmptyMessage (STOPSERVICE);
+                        mHandler.sendEmptyMessageDelayed ( STARTSERVICE, 3000 );
+                    } else if (netInfo.getState() == NetworkInfo.State.DISCONNECTED) {
+                        Log.d ( TAG ,"TypeName " + netInfo.getTypeName()+ " disconnected");
+                        mHandler.sendEmptyMessage (STOPSERVICE);
                     }
-                } if ( ( netInfo != null ) && ( netInfo.getDetailedState() == NetworkInfo.DetailedState.CONNECTED ) ) {
-                    cxt.stopService ( new Intent ( cxt, MediaCenterService.class ) );
-                    if ( mPrefUtils.getBooleanVal ( DmpStartFragment.KEY_BOOT_CFG, false ) ) {
-                        cxt.startService ( new Intent ( cxt, MediaCenterService.class ) );
-                    }
-                    if ( mPrefUtils.getBooleanVal ( SettingsPreferences.KEY_BOOT_CFG, false ) ) {
-                        cxt.startService ( new Intent ( cxt, AirPlayService.class ) );
-                    }
-                    Debug.d ( TAG , ">>>>>onReceive :network connected" );
+
+                } else {
+                    if (netInfoWIFI != null && netInfoETH != null)
+                        Log.d ( TAG ," No ActiveNetwork !"+ netInfoWIFI.getState() +"|" +netInfoETH.getState());
                 }
             }
             if ( ( WifiManager.WIFI_AP_STATE_CHANGED_ACTION ).equals ( intent.getAction() ) ) {
                 int wifi_AP_State =  intent.getIntExtra ( WifiManager.EXTRA_WIFI_AP_STATE, WifiManager.WIFI_AP_STATE_FAILED );
                 if ( WifiManager.WIFI_AP_STATE_ENABLED == wifi_AP_State ) {
                     cxt.stopService ( new Intent ( cxt, MediaCenterService.class ) );
-                    if ( mPrefUtils.getBooleanVal ( DmpStartFragment.KEY_BOOT_CFG, false ) ) {
-                        cxt.startService ( new Intent ( cxt, MediaCenterService.class ) );
-                    }
-                    if ( AirPlayService.STATEDFLAG &&  mPrefUtils.getBooleanVal ( SettingsPreferences.KEY_BOOT_CFG, false ) ) {
-                        cxt.startService ( new Intent ( cxt, AirPlayService.class ) );
-                    }
+                    mHandler.sendEmptyMessage (STOPSERVICE);
+                    mHandler.sendEmptyMessageDelayed ( STARTSERVICE, 3000 );
                 } else {
-                    Intent mNetIntent = new Intent ( DmpService.NETWORK_ERROR );
-                    cxt.sendBroadcast ( mNetIntent );
-                    cxt.stopService ( new Intent ( cxt, AirPlayService.class ) );
+                    mHandler.sendEmptyMessage (STOPSERVICE);
                 }
             }
-            /*this broadcast is useless than network change*/
+            //this broadcast is useless than network change
             if ( intent.getAction().equals ( Intent.ACTION_BOOT_COMPLETED ) ) {
                 //SharedPreferences prefs = Utils.getSharedPreferences(cxt);
                 //boolean autostart  = prefs.getBoolean(SettingsPreferences.KEY_BOOT_CFG, false);
@@ -100,4 +102,31 @@ public class DMRBroadcastReceiver extends BroadcastReceiver {
                 //}
             }
         }
+        private Handler mHandler = new Handler() {
+            @Override
+            public void handleMessage ( Message msg ) {
+                switch ( msg.what ) {
+                    case STOPSERVICE:
+                        Intent mNetIntent = new Intent(DmpService.NETWORK_ERROR);
+                        mContext.sendBroadcast ( mNetIntent );
+                        if ( AirPlayService.STATEDFLAG && mPrefUtils.getBooleanVal ( SettingsPreferences.KEY_BOOT_CFG, false ) ) {
+                            Log.d ( TAG , " stopService AirPlayService");
+                            mContext.stopService(new Intent(mContext,AirPlayService.class));
+                        }
+                        break;
+                    case STARTSERVICE:
+                        if (mPrefUtils.getBooleanVal(DmpStartFragment.KEY_BOOT_CFG,false)) {
+                            mContext.startService (new Intent(mContext, MediaCenterService.class));
+                            Log.d ( TAG , " startService MediaCenterService");
+                        }
+                        if (mPrefUtils.getBooleanVal(SettingsPreferences.KEY_BOOT_CFG,false)) {
+                            mContext.startService(new Intent(mContext,AirPlayService.class));
+                            Log.d ( TAG , " startService AirPlayService");
+                        }
+                        break;
+                }
+                // TODO Auto-generated method stub
+                super.handleMessage ( msg );
+            }
+        };
 }
