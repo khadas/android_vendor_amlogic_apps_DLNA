544f4f4cec463879a724fa6ff4d7431439e835ee
Zhiwei Gong <zhiwei.gong@amlogic.com>
2016-10-19 14:11:55 +0800

    PD#133217:PD#132841:PD#132783:PD#132495: 1.fix 64bit mode error 2.update LEBO-SDK-3.0.1.3r to support ios 10 & android n
    Change-Id: I7e1fee1dfc6fec79a1da85724b8abd3a597674bf

--- Android.mk
+++ Android.mk
@@ -10,27 +10,30 @@ LOCAL_PACKAGE_NAME := DLNA
 LOCAL_CERTIFICATE := platform
 LOCAL_PROGUARD_ENABLED := disabled
 LOCAL_PRIVILEGED_MODULE := true
-LOCAL_REQUIRED_MODULES := libhpplayaudio.so \
-                          libhpplaysmdns.so \
-                          libhpplaymdns.so \
-                          libhpplaymirror.so \
-                          libhpplayvideo.so \
-                          libhpplayvideo19.so \
-                          libhisivideo.so \
-                          libhisivideo19.so \
-                          libhisivideo_3798m.so \
-                          libplatinum-jni.so
+LOCAL_PREBUILT_JNI_LIBS := \
+                          libs/armeabi-v7a/libhpplayaudio.so \
+                          libs/armeabi-v7a/libhpplaysmdns.so \
+                          libs/armeabi-v7a/libhpplaymdns.so \
+                          libs/armeabi-v7a/libhpplaymirror.so \
+                          libs/armeabi-v7a/libhpplayvideo.so \
+                          libs/armeabi-v7a/libhpplayvideo19.so \
+                          libs/armeabi-v7a/libhisivideo.so \
+                          libs/armeabi-v7a/libhisivideo19.so \
+                          libs/armeabi-v7a/libhisivideo_3798m.so \
+                          libs/armeabi-v7a/libplatinum-jni.so
 #LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+LOCAL_MULTILIB := 32
 include $(BUILD_PACKAGE)
 
 ##############################################
 
 include $(CLEAR_VARS)
-LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := liblebo:libs/LEBO-SDK-3.0.0.7r_external_amlogic.jar \
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := liblebo:libs/LEBO-SDK-3.0.1.3r_external_amlogic.jar \
                                         libmid:libs/mid-mid-sdk-2.3.jar \
                                         libmta:libs/mta-android-stat-sdk-2.1.0_20160111.jar \
                                         libandroid-support-v4:libs/android-support-v4.jar \
                                         libdlna:libs/dlna.jar
+LOCAL_MULTILIB := 32
 include $(BUILD_MULTI_PREBUILT)
 
 
@@ -40,63 +43,71 @@ include $(BUILD_MULTI_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE := libhpplayaudio.so
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
+LOCAL_MODULE_PATH := $(TARGET_OUT)/lib#$(TARGET_OUT_SHARED_LIBRARIES)
 LOCAL_SRC_FILES := libs/armeabi-v7a/$(LOCAL_MODULE)
 OVERRIDE_BUILD_MODULE_PATH := $(TARGET_OUT_INTERMEDIATE_LIBRARIES)
+LOCAL_MULTILIB := 32
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libhpplaysmdns.so
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
+LOCAL_MODULE_PATH := $(TARGET_OUT)/lib #$(TARGET_OUT_SHARED_LIBRARIES)
 LOCAL_SRC_FILES := libs/armeabi-v7a/$(LOCAL_MODULE)
 OVERRIDE_BUILD_MODULE_PATH := $(TARGET_OUT_INTERMEDIATE_LIBRARIES)
+LOCAL_MULTILIB := 32
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libhpplaymdns.so
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
+LOCAL_MODULE_PATH := $(TARGET_OUT)/lib #$(TARGET_OUT_SHARED_LIBRARIES)
 LOCAL_SRC_FILES := libs/armeabi-v7a/$(LOCAL_MODULE)
 OVERRIDE_BUILD_MODULE_PATH := $(TARGET_OUT_INTERMEDIATE_LIBRARIES)
+LOCAL_MULTILIB := 32
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libhpplayvideo.so
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
+LOCAL_MODULE_PATH := $(TARGET_OUT)/lib #$(TARGET_OUT_SHARED_LIBRARIES)
 LOCAL_SRC_FILES := libs/armeabi-v7a/$(LOCAL_MODULE)
 OVERRIDE_BUILD_MODULE_PATH := $(TARGET_OUT_INTERMEDIATE_LIBRARIES)
+LOCAL_MULTILIB := 32
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libhpplayvideo19.so
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
+LOCAL_MODULE_PATH := $(TARGET_OUT)/lib #$(TARGET_OUT_SHARED_LIBRARIES)
 LOCAL_SRC_FILES := libs/armeabi-v7a/$(LOCAL_MODULE)
 OVERRIDE_BUILD_MODULE_PATH := $(TARGET_OUT_INTERMEDIATE_LIBRARIES)
+LOCAL_MULTILIB := 32
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libhisivideo.so
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
+LOCAL_MODULE_PATH := $(TARGET_OUT)/lib #$(TARGET_OUT_SHARED_LIBRARIES)
 LOCAL_SRC_FILES := libs/armeabi-v7a/$(LOCAL_MODULE)
 OVERRIDE_BUILD_MODULE_PATH := $(TARGET_OUT_INTERMEDIATE_LIBRARIES)
+LOCAL_MULTILIB := 32
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libhisivideo19.so
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
+LOCAL_MODULE_PATH := $(TARGET_OUT)/lib #$(TARGET_OUT_SHARED_LIBRARIES)
 LOCAL_SRC_FILES := libs/armeabi-v7a/$(LOCAL_MODULE)
 OVERRIDE_BUILD_MODULE_PATH := $(TARGET_OUT_INTERMEDIATE_LIBRARIES)
+LOCAL_MULTILIB := 32
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libhisivideo_3798m.so
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
+LOCAL_MODULE_PATH := $(TARGET_OUT)/lib #$(TARGET_OUT_SHARED_LIBRARIES)
 LOCAL_SRC_FILES := libs/armeabi-v7a/$(LOCAL_MODULE)
 OVERRIDE_BUILD_MODULE_PATH := $(TARGET_OUT_INTERMEDIATE_LIBRARIES)
+LOCAL_MULTILIB := 32
 include $(BUILD_PREBUILT)
